fc55200f80475eae1c2c5b631d6ff36d47111380
anvoja <anezka.musilova@seznam.cz>
2026-05-01 21:28:53 +0200

    camera

--- shaders/shader.vert
+++ shaders/shader.vert
@@ -2,12 +2,16 @@
 
 in vec2 inPosition;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main() {
     float x = inPosition.x;
     float y = inPosition.y;
+    float z = sin(5.0 * x) * cos(5.0 * y) * 0.3;
 
-    // SAFE wave (small amplitude!)
-    float z = sin(5.0 * x) * cos(5.0 * y) * 0.2;
+    vec4 position = vec4(x, y, z, 1.0);
 
-    gl_Position = vec4(x, y, z, 1.0);
+    gl_Position = projection * view * model * position;
 }
--- src/project/Renderer.java
+++ src/project/Renderer.java
@@ -4,18 +4,25 @@ import helpers.AbstractRenderer;
 import lwjglutils.OGLBuffers;
 import lwjglutils.OGLUtils;
 import lwjglutils.ShaderUtils;
+import org.lwjgl.glfw.GLFWCursorPosCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
+import transforms.*;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL20.glDeleteProgram;
-import static org.lwjgl.opengl.GL20.glUseProgram;
+import static org.lwjgl.opengl.GL20.*;
 
 public class Renderer extends AbstractRenderer {
     private OGLBuffers buffers;
     private int renderMode = GL_POINTS;
     private int shaderProgram;
+    private Mat4 projection;
+    private Mat4 view;
+    private Mat4 model;
+    private int locProjection;
+    private int locView;
+    private int locModel;
 
     private GLFWKeyCallback   keyCallback = new GLFWKeyCallback() {
         @Override
@@ -25,13 +32,17 @@ public class Renderer extends AbstractRenderer {
             if (action == GLFW_PRESS || action == GLFW_REPEAT){
                 switch (key) {
                     case GLFW_KEY_M:
-                        renderMode = GL_POINTS;
+                        renderMode = GL_TRIANGLE_STRIP;
+                        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
                         break;
                     case GLFW_KEY_N:
-                        renderMode = GL_LINES;
+                        renderMode = GL_TRIANGLES;
+                        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
                         break;
+
                     case GLFW_KEY_B:
                         renderMode = GL_TRIANGLES;
+                        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
                         break;
                 }
             }
@@ -44,6 +55,7 @@ public class Renderer extends AbstractRenderer {
         OGLUtils.printLWJLparameters();
         OGLUtils.printJAVAparameters();
         OGLUtils.shaderCheck();
+        glPointSize(5f);
         // grid resolution
         int N = 20;
 
@@ -60,24 +72,61 @@ public class Renderer extends AbstractRenderer {
             }
         }
 
-        int[] indexBufferData = {
-                0, 1, 2
-        };
+        int[] indexBufferData = new int[(N - 1) * (N - 1) * 6];
+        index = 0;
+
+        for (int y = 0; y < N - 1; y++) {
+            for (int x = 0; x < N - 1; x++) {
+                int i = y * N + x;
+
+                indexBufferData[index++] = i;
+                indexBufferData[index++] = i + 1;
+                indexBufferData[index++] = i + N;
+
+                indexBufferData[index++] = i + 1;
+                indexBufferData[index++] = i + N + 1;
+                indexBufferData[index++] = i + N;
+            }
+        }
 
         OGLBuffers.Attrib[] attributes = {
           new OGLBuffers.Attrib("inPosition", 2, 0)
         };
 
-        buffers = new OGLBuffers(vertexBufferData, 2, attributes, null);
+        buffers = new OGLBuffers(vertexBufferData, 2, attributes, indexBufferData);
 
         shaderProgram = ShaderUtils.loadProgram("/shader");
+        locProjection = glGetUniformLocation(shaderProgram, "projection");
+        locView = glGetUniformLocation(shaderProgram, "view");
+        locModel = glGetUniformLocation(shaderProgram, "model");
+        System.out.println("locProjection = " + locProjection);
+        System.out.println("locView = " + locView);
+        System.out.println("width = " + width + ", height = " + height);
     }
 
     @Override
     public void display() {
-        glUseProgram(shaderProgram);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glViewport(0, 0, width, height);
+        if (height == 0) return;
+        projection = new Mat4PerspRH(
+                60,
+                (double) width / height,
+                0.1,
+                100.0
+        );
+        view = new Mat4ViewRH(
+                new Vec3D(1.5, 0, -2),
+                new Vec3D(0, 0, 0),
+                new Vec3D(0, 0, 1)
+        );
+        model = new Mat4Identity();
+
+        glUseProgram(shaderProgram);
+        glUniformMatrix4fv(locModel, false, model.floatArray());
+        glUniformMatrix4fv(locProjection, false, projection.floatArray());
+        glUniformMatrix4fv(locView, false, view.floatArray());
+
         buffers.draw(renderMode, shaderProgram);
     }
 
@@ -85,4 +134,11 @@ public class Renderer extends AbstractRenderer {
     public GLFWKeyCallback getKeyCallback() {
         return keyCallback;
     }
+
+    protected GLFWCursorPosCallback cursorPosCallback = new GLFWCursorPosCallback() {
+        @Override
+        public void invoke(long window, double x, double y) {
+            // System.out.println("Cursor position [" + x + ", " + y + "]");
+        }
+    };
 }
